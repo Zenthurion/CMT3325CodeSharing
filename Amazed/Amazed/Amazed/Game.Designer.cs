10f8f61bb9fe67f6ebca12142a497c560b96dc0f
Dainius <dk471@live.mdx.ac.uk>
2015-02-10 13:48:32 +0000

    Constants

--- Amazed/Amazed/Amazed/Game.Designer.cs
+++ Amazed/Amazed/Amazed/Game.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Amazed
 {
-    partial class Form1
+    partial class Game
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(949, 467);
+            this.Name = "Game";
+            this.Text = "Amazed";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
